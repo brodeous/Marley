5457c447f74fa4feb4039f00fa8ed0e7d6bd8346
Noah Meeker <noahmeeker@gmail.com>
2024-02-06 20:23:48 -0800

    figured out empty string printing issue.

---
+++ app/services/cleanQueryParams.ts
@@ -0,0 +1,9 @@
+const cleanQueryParams = (url: string) => {
+    const parsed = new URL(url);
+    parsed.search = '';
+    return parsed.toString();
+}
+
+export {
+    cleanQueryParams
+}
--- app/services/jobs.ts
+++ app/services/jobs.ts
@@ -1,12 +1,15 @@
 import { scrap } from './puppeteer.js'
 import { sendMessage } from './discord.js'
 import { outputMessage } from './messenger.js'
+import { cleanQueryParams } from './cleanQueryParams.js'
 
 const testJob = async () => {
     const url = 'https://www.facebook.com/marketplace/109455995738828/search/?query=ford%20ranger%204x4';
-    const selector = '.x1i10hfl';
+    const selector = 'a[role="link"]';
 
-    const links = await scrap(url, selector);
+    const linksRaw = await scrap(url, selector);
+    //console.log(linksRaw);
+    const links = linksRaw.map(cleanQueryParams);
 
     const message = outputMessage(links.join('\n'));
 
