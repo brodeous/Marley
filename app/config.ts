e144727bfd02dd3b604274262ffb6901436d3fbc
NMeekGit <noahmeeker@gmail.com>
2024-02-08 16:58:47 -0800

    added simple web server init

--- app/config.ts
+++ app/config.ts
@@ -4,3 +4,4 @@ export const CLIENT_ID = process.env.CLIENT_ID || "";
 
 export const DISCORD_KEY = process.env.DISCORD_API_KEY || "";
 
+export const PORT = process.env.PORT || "";
--- app/index.ts
+++ app/index.ts
@@ -1,4 +1,17 @@
+import { createServer } from 'http';
+import { PORT } from './config';
 import { initializeDiscord, sendMessage } from "./services/discord.js";
 
 
 const client = await initializeDiscord();
+
+const server = createServer((req, res) => {
+    res.write('ok');
+    res.end();
+});
+
+const runServer = () => {
+    server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+}
+
+runServer();
